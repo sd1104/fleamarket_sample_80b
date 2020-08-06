1b6d8b89e273605ed1ad6ac8476691385dc9db25
bian160928 <biankobayashi@gmail.com>
2020-08-06 20:30:36 +0900

    make_up_routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
+
+  resouces :users
+  resouces :profiles
+  resouces :product_addresses
+  resouces :credits
+  resouces :items do
+    collection :categories
+  end
+  resouces :item_images
 end
