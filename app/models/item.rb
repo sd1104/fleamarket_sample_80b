05b782420da0a969e1275023f02c30f6f4858262
bian160928 <biankobayashi@gmail.com>
2020-08-06 20:15:33 +0900

    create_asocietion

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,7 @@
 class Item < ApplicationRecord
-  belongs_to :category
+  belongs_to :category, dependent: :destroy
   belongs_to :seller, class_name: "User", foreign_key: "seller_id"
   belongs_to :buyer, class_name: "User", foreign_key: "buyer_id"
+  has_many :item_images, dependent: :destroy
+  belongs_to :user, dependent: :destroy
 end
--- app/models/product_address.rb
+++ app/models/product_address.rb
@@ -1,2 +1,3 @@
 class ProductAddress < ApplicationRecord
+  belongs_to :user, dependent: :destroy
 end
