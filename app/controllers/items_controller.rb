f7b1352bf3106a2d151a26ce4b354f52d57f584c
bian160928 <biankobayashi@gmail.com>
2020-08-07 13:01:49 +0900

    make_up_user_controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,23 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
+
+  def new 
+    @item =Item.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+
 end
