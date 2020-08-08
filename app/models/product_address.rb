b67c413bc2f71a35aa5bf3301b16921b06f91b30
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-08-08 17:14:30 +0900

    prefecture_select_box implemented by jp_prefecture

--- app/models/product_address.rb
+++ app/models/product_address.rb
@@ -1,4 +1,15 @@
 class ProductAddress < ApplicationRecord
+  include JpPrefecture
+  jp_prefecture :prefecture_code
+  
+  def prefecture_name
+    JpPrefecture::Prefecture.find(code: prefecture_code).try(:name)
+  end
+  
+  def prefecture_name=(prefecture_name)
+    self.prefecture_code = JpPrefecture::Prefecture.find(name: prefecture_name).code
+  end
+  
   belongs_to :user, dependent: :destroy
 
   validates :first_name, :last_name, :first_name_kana, :last_name_kana, :postal_code, :prefecture_code, :address_city, :address_street, 
