b7eb03a5da81719d526ea2ed5af609e401370e5d
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-08-08 17:10:00 +0900

    initial model settings done

--- app/models/product_address.rb
+++ app/models/product_address.rb
@@ -1,3 +1,8 @@
 class ProductAddress < ApplicationRecord
   belongs_to :user, dependent: :destroy
+
+  validates :first_name, :last_name, :first_name_kana, :last_name_kana, :postal_code, :prefecture_code, :address_city, :address_street, 
+    presence: true
+  validates :phone_number, 
+    uniqueness: true
 end
