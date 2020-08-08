007a1a6849393d94c7cdd230cd962df724aa1f82
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-08-08 17:09:30 +0900

    routes settings done

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'items#index'
-
+  # root 'items#index'
+  root 'product_addresses#index'
+  
   resources :users
   resources :profiles
-  resources :product_addresses
+  resources :product_addresses, only: [ :index, :new, :create ]
   resources :credits
   
   
