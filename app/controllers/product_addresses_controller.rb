ac5d2b5a04c6c28ace75383ee76836c5b0f15c89
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-08-08 17:09:47 +0900

    1st controller settings done

--- app/controllers/product_addresses_controller.rb
+++ app/controllers/product_addresses_controller.rb
@@ -1,2 +1,35 @@
 class ProductAddressesController < ApplicationController
+
+  def index
+    @product_addresses = ProductAddress.includes(:user)
+  end
+
+  def new
+    @product_addresses = ProductAddress.new
+  end
+  
+  def create
+    @product_addresses = ProductAddress.new(product_address_params)
+    if @product_addresses.create(product_address_params)
+      redirect_to product_addresses_path
+    else
+      render :new
+    end
+  end
+
+  private
+   def product_address_params
+    params.permit(
+      :first_name, 
+      :last_name, 
+      :first_name_kana, 
+      :last_name_kana, 
+      :postal_code, 
+      :prefecture_code, 
+      :address_city, 
+      :address_street, 
+      :building_name, 
+      :phone_number
+    ).merge(user_id: 8)
+   end
 end
