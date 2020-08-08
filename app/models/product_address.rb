de87672c756985cd0dff4b4bc788185b8bc6929e
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-08-08 18:15:57 +0900

    validation settings done

--- app/models/product_address.rb
+++ app/models/product_address.rb
@@ -9,11 +9,20 @@ class ProductAddress < ApplicationRecord
   def prefecture_name=(prefecture_name)
     self.prefecture_code = JpPrefecture::Prefecture.find(name: prefecture_name).code
   end
-  
+
   belongs_to :user, dependent: :destroy
 
   validates :first_name, :last_name, :first_name_kana, :last_name_kana, :postal_code, :prefecture_code, :address_city, :address_street, 
     presence: true
   validates :phone_number, 
-    uniqueness: true
+    uniqueness: true,
+    format: { with: /\A\d{10,11}\z/ }
+  validates :first_name, :last_name,
+    numericality: false,
+    format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :first_name_kana, :last_name_kana,
+    numericality: false,
+    format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :postal_code,
+    format: { with: /\A\d{7}\z/ }
 end
