567c25d846453f0657bad8101234b773cd1e8519
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-08-09 09:32:28 +0900

    codes for model revised

--- app/models/product_address.rb
+++ app/models/product_address.rb
@@ -14,15 +14,17 @@ class ProductAddress < ApplicationRecord
 
   validates :first_name, :last_name, :first_name_kana, :last_name_kana, :postal_code, :prefecture_code, :address_city, :address_street, 
     presence: true
-  validates :phone_number, 
-    uniqueness: true,
-    format: { with: /\A\d{10,11}\z/, message: "はハイフンなし、半角数字で入力してください" }
   validates :first_name, :last_name,
-    numericality: false,
     format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角で入力してください" }
   validates :first_name_kana, :last_name_kana,
-    numericality: false,
-    format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力してください。" }
+    format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力してください" }
   validates :postal_code,
     format: { with: /\A\d{7}\z/, message: "はハイフンなし、半角数字のみの７桁で入力してください" }
+  validates :phone_number,
+    allow_blank: true,
+    uniqueness: true,
+    format: { with: /\A\d{10,11}\z/, message: "はハイフンなし、半角数字で入力してください" }
 end
+
+
+/\A[+-]?\d+\z/
