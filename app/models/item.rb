d41e98b781cc93f0f3897030a2fdcdbf3ca18066
defminami <yo.minam-i@hotmail.co.jp>
2020-08-10 20:20:50 +0900

    multiple

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,13 +4,14 @@ class Item < ApplicationRecord
   belongs_to :buyer, class_name: "User", foreign_key: "buyer_id"
   has_many :item_images, dependent: :destroy
   belongs_to :user, dependent: :destroy
+  accepts_nested_attributes_for :item_images, allow_destroy: true, update_only: true
 
   validates :name, presence: true
   validates :introduction, presence: true
   validates :price, presence: true
-  validates :condition, presence: true
-  validates :delivery_charge, presence: true
-  validates :derivery_origin, presence: true
-  validates :delivery_date, presence: true
+  validates :condition_id, presence: true
+  validates :delivery_charge_id, presence: true
+  validates :delivery_origin_id, presence: true
+  validates :delivery_date_id, presence: true
   validates :category_id, presence: true
 end
--- db/migrate/20200806084340_create_items.rb
+++ db/migrate/20200806084340_create_items.rb
@@ -4,10 +4,10 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.string :name, null: false
       t.text :introduction, null: false
       t.string :brand
-      t.integer :condition, null: false
-      t.integer :delivery_charge, null: false
-      t.integer :derivery_origin, null: false
-      t.integer :delivery_date, null: false
+      t.string :condition_id, null: false
+      t.string :delivery_charge_id, null: false
+      t.string :delivery_origin_id, null: false
+      t.string :delivery_date_id, null: false
       t.bigint :category_id, null: false, foreign_key: true
       t.integer :price, null: false
       t.bigint :seller_id, null: false, foreign_key: true
