6a1aef06a0e89d0543b1ac43b1ecc51b7d5c963c
bian160928 <biankobayashi@gmail.com>
2020-08-10 20:52:12 +0900

    change

--- db/migrate/20200806082344_devise_create_users.rb
+++ db/migrate/20200806082344_devise_create_users.rb
@@ -4,7 +4,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
+<<<<<<< Updated upstream
       t.string :nickname,           null: false
+=======
+      t.string :nickname
+>>>>>>> Stashed changes
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
