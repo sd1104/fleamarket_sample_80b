bbc2fd9ae96db5640f3dc0de15058e559eed1a1a
yuriyanagi <40306yy@gmail.com>
2020-08-11 10:58:03 +0900

    makeup profile

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,12 @@
 class Profile < ApplicationRecord
   belongs_to :user, dependent: :destroy
+
+  VALID_NAME = /\A[ぁ-んァ-ン一-龥]/
+  VALID_NAME_KANA = /\A[ァ-ヶー－]+\z/
+
+  validates :first_name,      presence: true, format:{with: VALID_NAME, message:"は全角で入力してください"}
+  validates :last_name,       presence: true, format:{with: VALID_NAME, message:"は全角で入力してください"}
+  validates :first_name_kana, presence: true, format:{with: VALID_NAME_KANA, message:"は全角カタカナで入力してください"}
+  validates :last_name_kana,  presence: true, format:{with: VALID_NAME_KANA, message:"は全角カタカナで入力してください"}
+  validates :birthday,        presence: true
 end
