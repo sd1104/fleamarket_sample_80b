9655bcca06b6a3e1a0be9dc5371b7f377bb47abe
defminami <yo.minam-i@hotmail.co.jp>
2020-08-11 12:31:39 +0900

    commit0

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,16 +9,16 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
   end
 
-  # private
+  private
 
-  # def production?
-  #   Rails.env.production?
-  # end
+  def production?
+    Rails.env.production?
+  end
 
-  # def basic_auth
-  #   authenticate_or_request_with_http_basic do |username, password|
-  #     username == Rails.application.credentials[:basic_auth][:user] &&
-  #     password == Rails.application.credentials[:basic_auth][:pass]
-  #   end
-  # end
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == Rails.application.credentials[:basic_auth][:user] &&
+      password == Rails.application.credentials[:basic_auth][:pass]
+    end
+  end
 end
