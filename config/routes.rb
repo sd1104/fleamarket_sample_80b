23a6b2092f71478ba641a357b6118e23c86e2214
techss22 <jemus250@gmail.com>
2020-08-11 16:33:27 +0900

    route setting done

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
     resources :item_images
     collection do
       get 'search'
+      get 'category/get_category_children', to: 'items#get_category_children', defaults: { format: 'json' }
+      get 'category/get_category_grandchildren', to: 'items#get_category_grandchildren', defaults: { format: 'json' }
     end
   end
   resources :categories, only: :index
