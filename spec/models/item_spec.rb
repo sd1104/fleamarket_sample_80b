93eba6f3bf011c987f57fbfa257da1db6053b6c6
RyoIto0911 <night224@icloud.com>
2020-08-14 15:50:20 +0900

    item_test_1

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -6,9 +6,12 @@ describe Item do
 
     context 'can save' do
       
-      it 'is valid with name' do
-        @item = build(:item)
-        expect(@item).to be_valid
+      it 'is valid with all: name, introduction, price, condition_id, delivery_charge_id, delivery_origin_id, delivery_date_id, brand, category, seller' do
+        expect(build(:item)).to be_valid
+      end
+
+      it 'is valid with name, introduction, price, condition_id, delivery_charge_id, delivery_origin_id, delivery_date_id, category, seller' do
+        expect(build(:item, brand: nil)).to be_valid
       end
 
     end
@@ -17,12 +20,62 @@ describe Item do
 
     context 'can not save' do
 
+      it 'is invalid without seller' do
+        item = build(:item, seller: nil)
+        item.valid?
+        expect(item.errors[:seller]).to include("を入力してください")
+      end
+
       it 'is invalid without name' do
         item = build(:item, name: nil)
         item.valid?
         expect(item.errors[:name]).to include("を入力してください")
       end
+
+      it 'is invalid without introduction' do
+        item = build(:item, introduction: nil)
+        item.valid?
+        expect(item.errors[:introduction]).to include("を入力してください")
+      end
+
+      it 'is invalid without category' do
+        item = build(:item, category: nil)
+        item.valid?
+        expect(item.errors[:category]).to include("を入力してください")
+      end
+
+      it 'is invalid without condition_id' do
+        item = build(:item, condition_id: nil)
+        item.valid?
+        expect(item.errors[:condition_id]).to include("を入力してください")
+      end
+
+      it 'is invalid without delivery_charge_id' do
+        item = build(:item, delivery_charge: nil)
+        item.valid?
+        expect(item.errors[:delivery_charge_id]).to include("を入力してください")
+      end
+
+      it 'is invalid without delivery_origin_id' do
+        item = build(:item, delivery_origin_id: nil)
+        item.valid?
+        expect(item.errors[:delivery_origin_id]).to include("を入力してください")
+      end
+
+      it 'is invalid without delivery_date_id' do
+        item = build(:item, delivery_date_id: nil)
+        item.valid?
+        expect(item.errors[:delivery_date_id]).to include("を入力してください")
+      end
+
+      it 'is invalid wiyhout price' do
+        item = build(:item, price: nil)
+        item.valid?
+        expect(item.errors[:price]).to include("を入力してください")
+      end
+
       
+
     end
 
 
