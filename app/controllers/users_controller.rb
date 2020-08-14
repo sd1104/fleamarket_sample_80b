1e50d8db0ceeee06b284b27b78763948a83151bd
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-08-14 19:33:07 +0900

    controller settings done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,15 @@ class UsersController < ApplicationController
   end
 
   def item_exhibiting
+    @items_exhibiting = Item.includes(:item_images).where(seller_id: current_user.id, buyer_id: nil)
   end
 
   def item_sold
+    @items_exhibiting = Item.includes(:item_images).where(seller_id: current_user.id).where.not(buyer_id: current_user.id, buyer_id: nil)
   end
 
   def item_purchaced
+    @items_exhibiting = Item.includes(:item_images).where(buyer_id: current_user.id)
   end
   
 end
