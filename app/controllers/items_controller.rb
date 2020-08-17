ad8fe527844124fd888c13cb1ad59fa9c2009c01
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-08-17 20:22:07 +0900

    search for items Lv1 functions implemented

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,8 +36,6 @@ class ItemsController < ApplicationController
     end
   end
 
-  
-
   def edit
     @itemcategory = Category.where(ancestry: nil)
     @childrencategory = @item.category.parent.parent.children
@@ -51,7 +49,6 @@ class ItemsController < ApplicationController
       redirect_to edit_item_path(@item), notice: "必須項目を入力してください"
     end
   end
-
 
   def show
     @grandchild = Category.find(@item.category_id)
@@ -69,6 +66,11 @@ class ItemsController < ApplicationController
     end
   end
   
+  def search
+    @items = Item.search(params[:keyword])
+    # @items = Item.all
+  end
+
   private
 
   def item_params
